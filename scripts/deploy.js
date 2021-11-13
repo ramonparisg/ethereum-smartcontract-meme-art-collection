33fa3307f6368fa01006d52cbb34651e4440cc5a
Ramón París <rparis@retailsbs.com>
2021-11-13 17:06:45 -0300

    Creating local environment and deploy the smart contract

---
+++ scripts/deploy.js
@@ -0,0 +1,31 @@
+const main = async () => {
+  const [deployer] = await hre.ethers.getSigners();
+  const accountBalance = await deployer.getBalance();
+
+  console.log("Deploying contracts with address %s", deployer.address);
+  console.log("Account balance of address %s", accountBalance);
+
+  const contractFactory = await hre.ethers.getContractFactory(
+    "MemeArtCollectionPortal"
+  );
+  const contract = await contractFactory.deploy();
+  await contract.deployed();
+  console.log(
+    "Fuck yeaaaaaah. Contract deployed to:%s by %s",
+    contract.address
+  );
+
+  console.log("Account balance after deploy %s", await deployer.getBalance());
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
